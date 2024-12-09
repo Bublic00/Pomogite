528c12c09fd1602f4788881c76390ec893340343
Kosty <uferovkosta28@gmail.com>
2024-12-09 21:04:04 +0300

    Начата работа с окном где основной функционал ъыыъыъыъ

--- src/main/java/org/example/project/HelloApplication.java
+++ src/main/java/org/example/project/HelloApplication.java
@@ -11,10 +11,15 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(),Region.USE_COMPUTED_SIZE,Region.USE_COMPUTED_SIZE);
+        FXMLLoader fxmlLoader_Mainmenu = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader_WorkPlace = new FXMLLoader(HelloApplication.class.getResource("WorkPlace.fxml"));
+
+        Scene MainMenu = new Scene(fxmlLoader_Mainmenu.load(),Region.USE_COMPUTED_SIZE,Region.USE_COMPUTED_SIZE);
+        //Тута определяется окно рабочее)
+        Scene WorkPlace =new Scene(fxmlLoader_WorkPlace.load(),Region.USE_COMPUTED_SIZE,Region.USE_COMPUTED_SIZE);
+
         stage.setTitle("Hello!");
-        stage.setScene(scene);
+        stage.setScene(MainMenu);
         stage.show();
     }
 
